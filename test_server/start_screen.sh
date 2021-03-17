caef9432aac5106506abc300602ba946645a2262
tjm1518 <info@tmoor.es>
2021-03-17 09:32:45 +0000

    changed start server to support remote server

---
+++ test_server/start_screen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+erlc pong_app.erl pong_sup.erl pong_game.erl pong_server.erl &&\
+  screen erl -pa .build -name pong@$(dig +short myip.opendns.com @resolver1.opendns.com) -setcookie "let's play pong" -eval "application:start(pong)"
+
--- test_server/start_server.sh
+++ test_server/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#screen erl -name remnd@$(dig +short myip.opendns.com @resolver1.opendns.com) -setcookie infoproc -eval "compile:file(rm_node),application:start(rm_node)."
-erlc -o .build pong_app.erl pong_sup.erl pong_game.erl pong_server.erl && \
-erl -pa .build -sname pong -setcookie "let's play pong" -eval "application:start(pong)"
+erlc pong_app.erl pong_sup.erl pong_game.erl pong_server.erl &&\
+  erl -pa .build -name pong@$(dig +short myip.opendns.com @resolver1.opendns.com) -setcookie "let's play pong" -eval "application:start(pong)"
 
