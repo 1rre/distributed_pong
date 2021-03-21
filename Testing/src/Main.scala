b85acd15a29b289ee387a76b05ed3698abb20861
tjm1518 <54404449+tjm1518@users.noreply.github.com>
2021-03-21 23:04:45 +0000

    probably works but I can't test rn

--- Testing/src/Main.scala
+++ Testing/src/Main.scala
@@ -1,7 +1,8 @@
 
-import game.{Controller,Game}
+import game.Game
 import connection.OtpHelpers._
 import connection.Node._
+import nios.Interface
 import com.ericsson.otp.erlang._
 import language.postfixOps
 import scala.collection.immutable
@@ -10,31 +11,42 @@ object Main extends App {
   // Typedefs because those types are long haha
   type ErlTuple = OtpErlangTuple
   type ErlFloat = OtpErlangDouble
+  type ErlInt = OtpErlangLong
   type Atom = OtpErlangAtom
 
   // Make a new game
   val game = new Game
 
-  // (For testing only) make a new controller to control the kame with the keyboard
-  val controller = new Controller(game)
+  val nios = new Interface
 
   while (true) {
-    // Receive a message & decode it
-    connection.waitFor[ErlTuple].elementAt(1).asInstanceOf[ErlTuple].elements.toList match {
-      case List(ball: ErlTuple, players: ErlTuple) => {
-        ball.elements.toList match {
-          case List(x: ErlFloat, y: ErlFloat) =>
-            // Update the ball positions to those included in the message
-            game.Ball(x.doubleValue,y.doubleValue)
-          case other =>
-        }
-        // Update each of the players to the positions sent in the message
-        players.elements.zip(game.players).map(x => x._2(x._1))
+    if (connection.msgCount > 0) {
+      val msg = connection.waitFor[ErlTuple]
+      msg elementAt 0 match {
+        case ok: Atom if ok.atomValue == "ok" =>
+          (msg elementAt 1).asInstanceOf[ErlTuple].elements toList match {
+            case List(ball: ErlTuple, players: ErlTuple) => {
+              ball.elements.toList match {
+                case List(x: ErlFloat, y: ErlFloat) =>
+                  // Update the ball positions to those included in the message
+                  game.Ball(x.doubleValue,y.doubleValue)
+                case other =>
+              }
+              // Update each of the players to the positions sent in the message
+              players.elements.zip(game.players).map(x => x._2(x._1))
+            }
+            case other =>
+          }
+        case new_score: Atom if new_score.atomValue == "new_score" =>
+          val toNios = (msg elementAt 1).asInstanceOf[ErlInt].byteValue
+
+          nios.writeData write toNios
       }
-      case other =>
     }
-    // Print the buffer to reflect the new game state
-    print(game)
+    else if (nios.readData.available >= 2) {
+      val read = nios.readData read; nios.readData read;
+      connection.send("pong_server", (a"change_pos", pid, read) toOTP)
+    }
   }
 
 
---
+++ Testing/src/nios/Interface.scala
@@ -0,0 +1,16 @@
+package nios
+
+import java.io.{OutputStream,InputStream}
+import language.postfixOps
+import sys.process._
+
+class Interface {
+  val windows = sys.props.get("os.name") != Some("Linux") 
+  var writeData = null.asInstanceOf[OutputStream]
+  var readData = null.asInstanceOf[InputStream]
+  def nios2Input = (in: OutputStream) => writeData = in
+  def nios2Output: InputStream=>Unit = (out: InputStream) => readData = out
+  val nios2Io = new ProcessIO(nios2Input,nios2Output,(err: InputStream) => err close)
+  val nios2cmd = if (windows) "nios2-terminal.exe" else "nios2-terminal"
+  val nios2 = s"$nios2cmd -q --persistent --no-quit-on-ctrl-d" run nios2Io
+}
