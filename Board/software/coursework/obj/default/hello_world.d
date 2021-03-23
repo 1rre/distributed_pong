783afdc0c4221d711fa270b1286cb8fd87f87e8f
tjm1518 <54404449+tjm1518@users.noreply.github.com>
2021-03-23 17:00:59 +0000

    added win

---
+++ Board/software/coursework/obj/default/hello_world.d
@@ -0,0 +1,29 @@
+obj/default/hello_world.o: hello_world.c \
+ c\:/Users/tjmoo/infoproc/coursework/Board/software/coursework_bsp/system.h \
+ c\:/Users/tjmoo/infoproc/coursework/Board/software/coursework_bsp/linker.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\io.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\alt_types.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_up_avalon_accelerometer_spi.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\alt_types.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\sys\alt_dev.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\sys\alt_llist.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\priv\alt_dev_llist.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_up_avalon_accelerometer_spi_regs.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_avalon_timer_regs.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_avalon_timer.h \
+ c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\sys\alt_warning.h
+c\:/Users/tjmoo/infoproc/coursework/Board/software/coursework_bsp/system.h:
+c\:/Users/tjmoo/infoproc/coursework/Board/software/coursework_bsp/linker.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_avalon_pio_regs.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\io.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\alt_types.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_up_avalon_accelerometer_spi.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\alt_types.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\sys\alt_dev.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\sys\alt_llist.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\priv\alt_dev_llist.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_up_avalon_accelerometer_spi_regs.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_avalon_timer_regs.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\drivers\inc\altera_avalon_timer.h:
+c\:\users\tjmoo\infoproc\coursework\board\software\coursework_bsp\hal\inc\sys\alt_warning.h:
